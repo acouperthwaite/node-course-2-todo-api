f79b92a189839ce439de9f15a49fc44bc18e8277
Andrew Couperthwaite <acouperthwaite@gmail.com>
2018-04-29 22:01:48 -0600

    Setup app for herohu

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,9 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+// mongoose.connect('mongodb://localhost:27017/TodoApp');
+var dbuser = 'acouperthwaite';
+var dbpass = 'couper01';
+mongoose.connect(`mongodb://${dbuser}:${dbpass}@ds157475.mlab.com:57475/todoapp`)
 
 module.exports = {mongoose};
