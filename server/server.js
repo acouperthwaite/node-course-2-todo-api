dea5454d5e056feacc6f71158f71a1740af693f5
Andrew Couperthwaite <acouperthwaite@gmail.com>
2018-04-29 21:14:32 -0600

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@
 
 var express = require('express');
 var bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 var {mongoose} =require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -28,6 +29,27 @@ app.get('/todos', (req,res)=>{
     res.send({todos}); //sending an object allows you to add other custom fields, look at this later
   },(e)=>{
     res.status(400).send(e);
+  });
+});
+
+// GET /todos/1234324 (similar to find user by id)
+
+app.get('/todos/:id', (req,res)=>{
+  var id = req.params.id;
+  //validate ID using isValid
+    // if not valid, respond with 404 - send empty body (send())
+    if (!ObjectID.isValid(id)){
+      res.status(404).send();
+    }
+  // findById
+  Todo.findById(id).then((todo)=>{
+    if(!todo){
+      return res.status(404).send();
+    }
+
+    res.send({todo});
+  },(e)=>{
+    res.status(400).send();
   });
 });
 
