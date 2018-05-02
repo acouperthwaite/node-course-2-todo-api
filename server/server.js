bd4a28e00e36f6a46577e36a9f74df9ede8d4f6e
Andrew Couperthwaite <acouperthwaite@gmail.com>
2018-05-01 18:28:09 -0600

    Add PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,9 +1,11 @@
 //server.js
 //responsible for routes only
 
-var express = require('express');
-var bodyParser = require('body-parser');
+const _ = require('lodash');
+const express = require('express');
+const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
+
 
 var {mongoose} =require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -62,13 +64,6 @@ app.delete('/todos/:id', (req,res)=>{
   if(!ObjectID.isValid(id)){
     return res.status(404).send();
   }
-
-  // remove todo by id
-  // success
-    // if no doc, send 404
-    // if doc, send doc back with 200
-  // error
-    //400 with empty body
   Todo.findByIdAndRemove(id).then((todo)=>{
     if(!todo){
       return res.status(404).send();
@@ -77,6 +72,32 @@ app.delete('/todos/:id', (req,res)=>{
   },(e)=>{
     res.status(400).send();
   });
+});
+
+app.patch('/todos/:id',(req,res)=>{
+  var id = req.params.id;
+  //pick off only the elements we want the user to be able to update (not anything they want)
+  var body = _.pick(req.body, ['text','completed']);
+
+
+  if(!ObjectID.isValid(id)){
+    return res.status(404).send();
+  }
+  if (_.isBoolean(body.completed) && body.completed){
+    body.completedAt = new Date().getTime();
+  }else{
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+  Todo.findByIdAndUpdate(id,{$set:body},{new: true}).then((todo)=> {
+    if(!todo){
+      return res.status(404).send();
+    }
+    res.send({todo});
+  }).catch((e)=>{
+    res.status(400).send();
+  })
 });
 
 app.listen(port, ()=>{
