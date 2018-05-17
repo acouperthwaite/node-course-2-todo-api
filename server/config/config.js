d496acddd95dc7f0f34fb466df35b92ac1658d38
acouperthwaite <38387463+acouperthwaite@users.noreply.github.com>
2018-05-17 15:25:35 -0600

    Update config to use json file

--- server/config/config.js
+++ server/config/config.js
@@ -3,10 +3,23 @@ var dbuser = 'acouperthwaite';
 var dbpass = 'couper01';
 process.env.MONGODB_URI = `mongodb://${dbuser}:${dbpass}@ds157475.mlab.com:57475/todoapp`;
 
-if(env=== 'development'){
-  process.env.PORT = 3000
-  process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
-} else if (env === 'test'){
-  process.env.PORT = 3000
-  process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
+if (env === 'development' || env === 'test'){
+      var config = require('./config.json');
+      var envConfig = config[env];
+
+      Object.keys(envConfig).forEach((key)=>{
+        process.env[key] = envConfig[key];
+      });
 }
+//
+
+//  Above keeps test/dev vars out of the code and into config.json (which we don't upload to github nor heroku)
+//  Also allows for easy maintenance of key pairs (don't modify code, just update json file)
+
+// if(env=== 'development'){
+//   process.env.PORT = 3000
+//   process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
+// } else if (env === 'test'){
+//   process.env.PORT = 3000
+//   process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
+// }
